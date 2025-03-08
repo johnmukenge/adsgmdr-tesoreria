6c4333cc322036e824cdead172c3ee3666474922
john.mukenge <john.mukenge@emeal.nttdata.com>
2025-03-08 10:21:01 +0100

    retrieve entrate mocks data

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message:'Hello World!', app: 'ADSGMDR'});
+const entrate = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/entrate.json`)
+);
+
+app.get('/api/v1/entrate', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: entrate.length,
+        data: {
+            entrate: entrate,
+        }
     });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
