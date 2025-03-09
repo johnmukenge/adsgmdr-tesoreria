64024cb53145f20bc0d0c3867b6651c73d279f84
john.mukenge <john.mukenge@emeal.nttdata.com>
2025-03-09 08:35:56 +0100

    environment variable

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const usersRouter = require('./routes/usersRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev')); // middleware for logging requests
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')); // middleware for logging requests
+}
+
 app.use(express.json()); // middleware for parsing the body of the request
 app.use(express.static(`${__dirname}/public`)); // middleware for serving static files
 
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'}); // load environment variables from.env file
+
 const app = require('./index');
+
+//console.log(app.get('env'));// show environment variable --> development
+//console.log(process.env);// show all environment variables
+
+const port = process.env.PORT || 3000; // use environment variable PORT or default 3000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
