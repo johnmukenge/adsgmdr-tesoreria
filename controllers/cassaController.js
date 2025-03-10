6c859b4dc05197211b1ee1ceb6b11e0007d0bceb
john mukenge <johnsonmukenge@gmail.com>
2025-03-10 21:28:47 +0100

    making the api better: filtering

--- controllers/cassaController.js
+++ controllers/cassaController.js
@@ -3,7 +3,28 @@ const Cassa = require('./../models/cassaModel');
 const getAllCasse = async (req, res) => {
     console.log(req.requestTime);
     try {
-        const casse = await Cassa.find();
+        console.log(req.query);
+        // Build the query
+        // 1. Filtering
+        const queryObj = { ...req.query };
+            // Fields to exclude from the queryObj
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+            // remove the fields from the queryObj
+        excludeFields.forEach(el => delete queryObj[el]);
+
+        // 2. Advanced filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+        
+        // exact query: { tipoTesoreria: 'centrale', importo: { $gte: 1000 } }
+        // coming from api { tipoTesoreria: 'centrale', percentuale: { gte: '15' } }
+
+        const query = Cassa.find(JSON.parse(queryStr));
+        // Execute the query
+        const casse = await query;
+
+        // Send the response
         res.status(200).json({
             status: 'success',
             results: casse.length,
@@ -14,7 +35,7 @@ const getAllCasse = async (req, res) => {
     } catch (error) {
         res.status(404).json({
             status: 'fail',
-            message: error,
+            message: error.message,
         });
     }
 };
